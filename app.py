fbeb1bef56d45d21781f2c373c57a23ea33617a1
lysabacate <133353329+lysabacate@users.noreply.github.com>
2025-05-14 21:42:04 +0000

    terminando página de login e editando detalhe  pag registrar

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ from flask import Flask, render_template, flash
 app = Flask(__name__)
 
 @app.route('/registrar')
-def index():
+def registrar():
     return render_template('pagina-registrar.html')
+
+@app.route('/login')
+def login():
+    return render_template('pagina-login.html')
+
+
 
 if __name__ == '__main__':
     app.run()
